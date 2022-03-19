9f0ea2b2db837d3962fbc1c20634d45d8052bc88
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-03-19 16:40:23 +0330

    add unit test for user profile update

--- src/test/java/com/example/ecommerce/unitTests/UserAccountControllerTest.java
+++ src/test/java/com/example/ecommerce/unitTests/UserAccountControllerTest.java
@@ -5,6 +5,7 @@ import com.example.ecommerce.dao.UserDao;
 import com.example.ecommerce.model.User;
 import com.example.ecommerce.service.UserService;
 import com.example.ecommerce.web.UserAccountController;
+import com.example.ecommerce.web.dto.UserSignupDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -36,23 +38,40 @@ public class UserAccountControllerTest {
     @MockBean
     UserDao userDao;
 
+    User user = new User(
+            UUID.fromString("aae82711-1546-4118-aaa0-de58f2d3c18b"),
+            "fatemeh",
+            "karimi",
+            "09123456789",
+            "fatemehkarimi@gmail.com",
+            "$2a$10$EdJnVTWFWG4j9VorOuO7W.7fkn2AB/P.Od4TUbFO4x.vqraR80Tq2");
+
     @Test
     @WithMockUser(username="fatemehkarimi@gmail.com", password="admin")
     public void getUserProfile() throws Exception {
-        User user = new User(
-                UUID.fromString("aae82711-1546-4118-aaa0-de58f2d3c18b"),
-                "fatemeh",
-                "karimi",
-                "09123456789",
-                "fatemehkarimi@gmail.com",
-                "$2a$10$EdJnVTWFWG4j9VorOuO7W.7fkn2AB/P.Od4TUbFO4x.vqraR80Tq2");
-
         Mockito.when(userDao.findByEmail(user.getEmail())).thenReturn(user);
 
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/api/user/profile"))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(user.getEmail()));
+    }
 
+    @Test
+    @WithMockUser(username="fatemehkarimi@gmail.com", password="admin")
+    public void updateUserProfile() throws Exception {
+        Mockito.when(userDao.findByEmail(user.getEmail())).thenReturn(user);
+        Mockito.when(userService.update(Mockito.any())).thenReturn(user);
+
+        Object updateData = new Object() {
+            public String phoneNumber = "091122334455";
+        };
+
+        String json = objectMapper.writeValueAsString(updateData);
+        mockMvc.perform(MockMvcRequestBuilders
+                       .post("/api/user/profile/edit")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
