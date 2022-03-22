10f46c2350774c9a22a1000165feefc597e61e95
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-03-22 21:36:55 +0430

    make feature work for real data

--- src/test/java/com/example/ecommerce/Cucumber/glue/UserLoginSteps.java
+++ src/test/java/com/example/ecommerce/Cucumber/glue/UserLoginSteps.java
@@ -1,22 +1,25 @@
 package com.example.ecommerce.Cucumber.glue;
+import com.example.ecommerce.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
-import io.restassured.response.ValidatableResponse;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class UserLoginSteps {
     private final static String BASE_URI = "http://localhost";
     @LocalServerPort
     private int port;
 
-    private ValidatableResponse validatableResponse;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    private Response response;
     private String email;
     private String password;
 
@@ -31,10 +34,25 @@ public class UserLoginSteps {
     }
 
     @Given("i send a request to URL {string} with email = {string} and password = {string}")
-    public void i_send_a_request_to_url_with_email_and_password(String url, String email, String password) {
+    public void i_send_a_request_to_url_with_email_and_password(String endpoint, String user_email, String user_password)
+            throws Exception
+    {
+        this.email = user_email;
+        this.password = user_password;
+        Object loginData = new Object() {
+            public String email = user_email;
+            public String password = user_password;
+        };
+
+        String json = objectMapper.writeValueAsString(loginData);
+        this.response = requestSpecification().contentType(ContentType.JSON)
+                .body(json).post(endpoint);
     }
 
     @Then("the result is {string}")
     public void the_result_is(String string) {
+        this.response.then().assertThat().statusCode(200);
+        String message = this.response.then().extract().asString();
+        assertThat(message).isEqualTo("user is now logged in");
     }
 }
--- src/test/resources/UserLogin.feature
+++ src/test/resources/UserLogin.feature
@@ -2,12 +2,12 @@ Feature: User Login To Account
 	a registered user must be able to login with email and password
 
 	Scenario Outline: User Can Login To Account
-		Given i send a request to URL "/api/auth/" with email = "<email>" and password = "<password>"
+		Given i send a request to URL "/api/auth/login" with email = "<email>" and password = "<password>"
 		Then the result is "<result>"
 
 		Examples:
-			| email 					| password	| result		|
-			| fatemehkarimi@gmail.com	| pass123	| authenticated	|
-			| mohammadmanafi@yahoo.com	| pass123	| authenticated	|
+			| email 						| password	| result		|
+			| fatemehkarimi.1998@yahoo.com	| admin		| authenticated	|
+			| mohammadmanafi@yahoo.com		| admin		| authenticated	|
 
 
