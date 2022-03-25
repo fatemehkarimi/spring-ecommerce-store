6fddaa02e18ed7f0e4de34bb910b7009299015cd
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-03-26 00:40:11 +0430

    user can view product detail at url /api/prodcut/id

--- src/main/java/com/example/ecommerce/web/ProductController.java
+++ src/main/java/com/example/ecommerce/web/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.ecommerce.dao.ProductDao;
 import com.example.ecommerce.model.Product;
@@ -9,20 +10,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/")
 public class ProductController {
     @Autowired
     private ProductDao productDao;
 
 
-    @GetMapping
+    @GetMapping("/products")
     public ResponseEntity <List <Product> > getProductList() {
         List <Product> result = productDao.findAll();
         return new ResponseEntity <List <Product> > (result, HttpStatus.OK);
     }
     
+    @GetMapping("/product/{id}")
+    public ResponseEntity <Optional <Product> > getProduct(@PathVariable("id") long id) {
+        Optional<Product> result = productDao.findById(id);
+        return new ResponseEntity<Optional <Product> >(result, HttpStatus.OK);
+    }
 }
